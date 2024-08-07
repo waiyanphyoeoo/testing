3f632b88a449e30286ed4be58bbe3f4437d187a1
Wai Yan Phyoe Oo <waiyanphyoeoo13@gmail.com>
2024-08-07 13:49:15 +0630

    upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Thant Zin Tun ( Frontend Developer )
+      </h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Wai Yan Phyoe Oo( DevOps Engineer)
+      </h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Khun Lin Naing Oo ( Backend Developer )
+      </h1>
+    </main>
+  );
+}
